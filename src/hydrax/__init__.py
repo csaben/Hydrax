10c3466bca2d09339f073ce504ddde267569b690
csaben <clark@saben.org>
2024-01-19 07:05:42 -0500

    loss is accessible to user

--- src/hydrax/__init__.py
+++ src/hydrax/__init__.py
@@ -2,8 +2,9 @@ from importlib import resources
 
 from .core import get_trained_models, train
 from .unbatching import get_original_model, unbatch_model
+from .loss import loss
 
 version_file = resources.files(__package__).joinpath("VERSION.txt")
 __version__ = version_file.read_text(encoding="utf-8").strip()
 
-__all__ = ["train", "get_trained_models", "get_original_model", "unbatch_model"]
+__all__ = ["train", "get_trained_models", "get_original_model", "unbatch_model", "loss"]
